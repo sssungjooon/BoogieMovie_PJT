586e276c4b17377380b4ae95e9f70d3a1ee58f34
sssungjooon <dellojoon7@gmail.com>
2024-12-06 11:35:47 +0900

    241206_renewal

--- final_pjt_back/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='followings')
-
-    def __str__(self) :
-        return self.username
--- final_pjt_back/makejson/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MakejsonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'makejson'
